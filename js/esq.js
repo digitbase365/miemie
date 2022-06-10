e341dcd83727ed020873311b3a08bf9f133f1313
unknown <darlsele@gmail.com>
2022-06-10 15:50:17 +0100

    Nav menu dismiss on Navlinks click

--- js/esq.js
+++ js/esq.js
@@ -10,5 +10,28 @@ menu.addEventListener('click', ()=>{
     nav.classList.toggle('swish')
 })
 
+navlink.forEach((item) => {
+    item.addEventListener('click', ()=>{
+        menu.classList.toggle('swish')
+        burger.classList.toggle('swish')
+        nav.classList.toggle('swish')
+
+    });
+})
 
 // End of Morphing Burger Icon{}
+
+
+// WHATSAPP top BOT SCRIPT****************************************//
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 400) { 
+    document.getElementById("top").classList.add("scroll-visible");
+  } else { 
+    document.getElementById("top").classList.remove("scroll-visible");
+
+  }
+}
+
+// END OF WHATSAPP top BOT SCRIPT****************************************//
